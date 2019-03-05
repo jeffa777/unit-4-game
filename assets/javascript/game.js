3e8e91f460d1f36b95a9421294edb8f74b1e309a
Jeff Ambrosini <jeffambrosini@gmail.com>
2019-03-05 14:41:25 -0500

    fixed bugs/issues, game working, syle done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,71 +12,53 @@ var crystalnumb4;
 
 // random computer number options for game
 function randomnumbers() {
-randomstartnumb = Math.floor((Math.random() * 120) + 19);
-crystalnumb1 = Math.floor((Math.random() * 12) + 1);
-crystalnumb2 = Math.floor((Math.random() * 12) + 1);
-crystalnumb3 = Math.floor((Math.random() * 12) + 1);
-crystalnumb4 = Math.floor((Math.random() * 12) + 1);
+randomstartnumb = Math.floor(Math.random() * 101 + 19);
+crystalnumb1 = Math.floor(Math.random() * 12 + 1);
+crystalnumb2 = Math.floor(Math.random() * 12 + 1);
+crystalnumb3 = Math.floor(Math.random() * 12 + 1);
+crystalnumb4 = Math.floor(Math.random() * 12 + 1);
 console.log(crystalnumb1, crystalnumb2, crystalnumb3, crystalnumb4);
 }
 
-
+// reset/new game...
 function reset() {
     randomnumbers();
     userscore = 0;
-    $("#randomstartnumb").text(randomstartnumb);
-    $("#userscore").text(userscore);
+    $("#randomstartnumb").text("Score to match: " + randomstartnumb);
+    $("#userscore").text("Your score: " + userscore);
     $("#image1").attr("data-crystalvalue", crystalnumb1);
     $("#image2").attr("data-crystalvalue", crystalnumb2);
     $("#image3").attr("data-crystalvalue", crystalnumb3);
     $("#image4").attr("data-crystalvalue", crystalnumb4);
-    $("#wins").text(wins);
-    $('#losses').text(losses);
     }
     
-    
-    
-    function youWin() {
-        $(".winlose").text("You Win!")
-    if (userscore === randomstartnumb); {
-    wins++;
-    $("#wins").text("Wins: " + wins);
-    reset();
-    }
-}
-    function youLose() {
-        $(".winlose").text("You lose, try again.")
-    if (userscore > randomstartnumb); {
-    losses++;
-    $("#losses").text("Losses: " + losses);
-    reset();
-    } 
-    }
-
     
     reset();    
     $(".crystal-image").on("click",function(){
-        if (userscore >= randomstartnumb) {
-            return;
+        $(".winlose").text("");
+        var crystalvalue = $(this).attr("data-crystalvalue");
+        crystalvalue = parseInt(crystalvalue);
+        userscore += crystalvalue;
+        $("#userscore").text("Your score: " + userscore);
+        check ();
+       
+        // check score to display win or lose properly
+        function check () {
+        if (userscore === randomstartnumb) {
+            $(".winlose").text("You Win!");
+            wins++;
+            $("#wins").text(wins);
+            reset();
+        } else if (userscore > randomstartnumb) {
+            $(".winlose").text("You lose, try again.");
+            losses++;
+            $("#losses").text(losses);
+            // console.log(losses);
+            reset();
         }
-    var crystalvalue = $(this).attr("data-crystalvalue");
-    crystalvalue = parseInt(crystalvalue);
-    userscore += crystalvalue;
-    $("#userscore").text(userscore);
-    
-    if (userscore === randomstartnumb) {
-       youWin();
-    } else if (userscore > randomstartnumb) {
-        youLose();
     }
-
-    // $(".btn").on("click", function() {
-    //     reset();
-
+    
 });
 
-$(".btn").on("click", function() {
-    reset();
-})
 
 })
